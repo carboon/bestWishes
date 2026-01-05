b31fafb2fecb4975ec5e5f3ee7c931295049dbc4
carboon <404293420@qq.com>
2026-01-05 19:10:37 +0800

    bug修复，暂时没有配置apikey

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
+  // App Router 在 Next.js 14 中已经是稳定功能，不需要 experimental.appDir
 }
 
 module.exports = nextConfig
